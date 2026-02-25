4b6cb46f071b2e2a18967cda358819b943b30dfc
Dipesh Sharma <dipeshsharmagautam@outlook.com>
2026-02-25 13:10:36 +0530

    add the cron task of every day run cron tab if exceeded 30 days it'l become a closed

---
+++ services/cron-task.js
@@ -0,0 +1,31 @@
+import cron from "node-cron";
+import connect from "../db.js";
+cron.schedule("0 0 * * *", async () => {
+  const { rows } = await connect.query("select created_at, uid from jobs");
+  const todayDate = new Date().toLocaleDateString();
+  const newMap = rows.map((row) => ({
+    created_at: row.created_at.toLocaleDateString(),
+    uid: row.uid,
+  }));
+  try {
+    const checkWhichNeedUpdate = newMap.filter((n) => {
+      if (n.created_at < todayDate) {
+        return { n };
+      }
+    });
+    if (checkWhichNeedUpdate.length > 0) {
+      for (let i of checkWhichNeedUpdate) {
+        await connect.query(
+          "update jobs set is_job_open='closed' where uid=$1",
+          [i.uid],
+        );
+      }
+    }
+    console.log("data cleaned successfuly");
+  } catch (error) {
+    console.log(error);
+    res.status(201).json({ message: error.message });
+  }
+});
+
+export default cron;
