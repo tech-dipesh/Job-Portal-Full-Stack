4d7262241e7a2e52b7b72ceb48156666877420c0
Dipesh Sharma <dipeshsharmagautam@outlook.com>
2026-02-26 19:18:44 +0530

    Add the entire logic of teh verify code of both email verification and also the reset password, resend verification code, with forget email password, with verify forget passsword with all teh validation and the edge case

--- controllers/verifyCode.controller.js
+++ controllers/verifyCode.controller.js
@@ -1,26 +1,31 @@
 import connect from "../db.js";
 import jwt from "jsonwebtoken"
+import sendMail from "../services/email-verification.js";
+import userSchema from "../Models/users.models.js";
+import SendmailTransport from "nodemailer/lib/sendmail-transport/index.js";
+import bcrypt from "bcryptjs";
+import currentDate from "../utils/currentDateFormat.js";
 async function verifyEmailConfirmation(req, res) {
+const {uid}=req.user;
   try {
-    const {token}=req.cookies;
-    const {userVerified}= jwt.verify(token, process.env.JSON_SECRET_KEY); 
-    if(token && userVerified==true){
-        res.status(204).json({message: "User Is Already Verified"})
+    const {code}=req.body;
+    if(!code){
+      return res.status(404).json({message: "Please Enter Verification Code"})
     }
-    
-    const {code, email}=req.body;
-    if(!code || !email){
-      return res.status(404).json({message: "Please Enter Verification Code and Email"})
-    }
-    console.log(code, email)
-    const {rows} =await connect.query("select e.verified_code, e.expired_at e.uid, u.email from email_verified e inner join users u on u.uid=e.user_id")
-    const filterThatMail=rows.filter(row=>row.email==email)
-    if(filterThatMail.length===0){
-      return res.status(201).json({message: "Please Enter Correct Code"})
-    }
-    const {uid: userId}=filterThatMail[0];
-   await connect.query(`update email_verified set is_verified=true where uid=$1`, [userId]);
-    return res.json({message: "Verification Code Have Been Succssfully Verified"})
+  const {rows} =await connect.query("select e.verified_code, e.is_verified, e.expired_at, e.uid, u.email from email_verified e inner join users u on u.uid=e.user_id where e.user_id=$1 order by e.created_at desc", [uid])
+  if(rows[0].verified_code!=code){
+    console.log(rows[0].verified_code)
+      return res.status(401).json({message: "Please Enter Correct Code"})
+  }
+  if(rows[0].is_verified==true){
+      return res.status(401).json({message: "Token Already In User, You've Already Logged In."})
+  }
+ 
+  if(rows[0].expired_at<currentDate){
+      return res.status(403).json({message: "Token is Expired Please Generate new Token"})
+  }
+  await connect.query(`update email_verified set is_verified=true where user_id=$1 and verified_code=$2`, [uid, code]);
+  return res.json({message: "Verification Code Have Been Succssfully Verified"})
   } catch (error) {
     console.log(error)
     return res.status(201).json({message: error.message})
@@ -28,8 +33,79 @@ async function verifyEmailConfirmation(req, res) {
 }
 
 
-export const resendVerificationCode=(req, res)=>{
 
-  res.status(201).json({message: 'hello'})
+export const resendVerificationCode=async (req, res)=>{
+  const {uid}=req.user;
+  if(!uid){
+    return res.status(201).json({message: "Please First Logged In"});
+  }
+  try {
+    const {rowCount}=await connect.query("select is_verified from email_verified where is_verified=true and user_id=$1", [uid])
+    if(rowCount>0){
+      return res.status(201).json({message: "You've Already Logged In Don't need to logged in again."});
+    }
+    const {rows}=await connect.query("select fname, lname, email from users where uid=$1", [uid])
+    const {fname, lname, email}=rows[0];
+     sendMail(uid, fname, lname, email, 'verify');
+    res.status(201).json({message: 'Resend Verification Code Have Been sent to your mail'})
+  } catch (error) {
+    console.log(error)
+  return  res.status(201).json({message: error.message})
+  }
 }
-export default verifyEmailConfirmation;
+
+
+export const forgetEmailPassword=async (req,res)=>{
+  const {email}=req.body;
+  if(!email){
+    return res.status(201).json({message: "Please Enter Email"});
+  }
+  try {
+    
+    const {rows, rowCount}=await connect.query("select fname, lname, uid from users where email=$1", [email])
+    if(rowCount===0){
+      return res.status(201).json({message: "Please Enter Correct or the Valid Email"})
+    }
+    const {fname, lname, uid}=rows[0];
+    sendMail(uid, fname, lname, email, 'forget')
+    
+    res.status(201).json({message: 'The password Have Beeen forget you can check your email for verify your code.'})
+  } catch (error) {
+    console.log(error)
+    return res.status(401).json({message: error.message})
+  }
+}
+
+export const verifyForgetPassword=async (req,res)=>{
+  const {code, newpassword, email}=req.body;
+  if(!code || !newpassword || !email){
+    return res.status(201).json({message: "Please Enter Your Code For Verification also a new passwoord and email"})
+  }
+  try {
+    
+    const {rowCount: userCount}=await connect.query("select email from users where email=$1", [email]);
+    if(userCount==0){
+      return res.status(201).json({message: "User is not logged in please logged in first."})
+    }
+    
+    const {rows: rowList, rowCount}=await connect.query("select u.password, u.email, e.verified_code, e.uid, e.is_verified from users u inner join email_verified e on e.user_id=u.uid  where u.email=$1 and e.verified_code=$2 order by e.expired_at desc limit 1;", [email, code])
+    const {uid}=rowList[0];
+    if(rowList[0].is_verified==true){
+      return res.status(403).json({message: "You've already forget your password from this token."})
+    }
+    if(rowCount==0){
+      return res.status(201).json({message: "Code Doesn't Match that youre looking for"})
+    }
+    if(rowList[0].expired_at<currentDate){
+      return res.status(403).json({message: "Token is Expired Please Generate new Token"})
+    }
+    const hashPassword=await bcrypt.hash(newpassword, 12)
+   const {rows}= await connect.query("update users set password=$1 where email=$2 returning fname, lname", [hashPassword, email])
+    await connect.query("update email_verified set is_verified=true where uid=$1", [uid])
+    res.status(201).json({message: `You: ${rows[0].fname}, ${rows[0].lname} Password have been updated`})
+  } catch (error) {
+    console.log(error)
+    return res.status(201).json({message: error.message})
+  }
+  }
+  export default verifyEmailConfirmation;
