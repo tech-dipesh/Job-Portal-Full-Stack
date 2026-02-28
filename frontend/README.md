77d903bff921d2a97de5287ef9db12356538d896
Dipesh Sharma <dipeshsharmagautam@outlook.com>
2026-02-28 14:00:53 +0530

    dd teh package file and also the vite config and also teh app jsx with only a header and the outlook

--- frontend/README.md
+++ frontend/README.md
@@ -2,7 +2,29 @@
 - the proejct is started on: 2026/02/26
 
 
-- 
+## frontend projects:
+- when using teh children property i must use the: `outlet` on the app.
+- as it's another concnetp that i''ve learn is for accessing a .env content i must start with: `vite` then i only can access a vite cotnte. with also must be the: `VITE` capital
+- i make sreut that it only rerender when the jobid is changed.
+- now i've change on the backend of validate a uid.
+- now i also start for login signup flow.
+- get method doesn't send anything on the body so for login i've to make the post rotues.
+- i make sure that with: `withCredentiasls` store the token from the backend.
+- i've also setup all teh signup feature here.
+
+- i make sure to create a new separate page for each data validation on the frontend.
+- the one major issue that i'm sendina  requeest to the: `login instead of verify`  
+
+- axiosi instance that i'll be usin ght `services` fodler.
+
+
+
+<!-- all backend route -->:
+- applications: `lists, id/apply, id/withdraw, `
+- companies: `/, id/dashbaord, id/employees, id/jobs, id/applications, id: get company all, id: post new company, / delete company, /id put new company`
+- jobs: `saved_jobs/list, id/bookmark_job, id/remove_from_bookmar, /: all listing, /search, id/ particular listing jobs, id: new jobs, id/ deelete, /id update`
+- users: `/logout, /login, /signup, /: all user, /:id individual user details, /skills add the user skills, /id, delete a user, /put, update user, /patch, update particular list, /forget-password, /forget-password-verify, /verify, /verify/resend, /upload-resume, /upload-profile-picture`
 
 ## Libraries Used:
 1. axios: for data fetching:
+2. tailwindcss: for styling
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,84 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import axios from "axios";
+import { Link, Outlet, useNavigate } from 'react-router';
+import isUserLoggedIn from './api/isUserLoggedin';
+import {logoutUser} from "./api/Login"
 
 function App() {
+  const navigate=useNavigate()
+  const [isLoggedUser, setUserLogged] = useState({ login: false, verify: false });
+  const checkUserLogged = async () => {
+    try {
+      const { status, data } = await isUserLoggedIn();
+      console.log('status', status, data)
+      setUserLogged({ login: true, verify: true })
+    } catch (error) {
+      if (!error.response){
+        setUserLogged({verify: false, login: false})
+      }
+      console.log('value', error.response)
+       const { status, data } = error.response
+      if (status == 401 && data?.message == "Please Verify Your Send to your mail.") {
+        setUserLogged({verify: false, login: true})
+      }
+      else{
+        setUserLogged({verify: false, login: false})
+      }
+    }
+  }
+  useEffect(() => {
+    checkUserLogged()
+  }, [])
+  
+  useEffect(() => {
+    if (isLoggedUser.login === true && isLoggedUser.verify === false) {
+    const timer= setTimeout(() => {
+    navigate("/verify-email")
+  }, 2000)
+  return ()=>{
+    clearTimeout(timer)
+  }
+}
+  }, [isLoggedUser])
 
- return (
-  <div>
-    
-  </div>
- )
+
+ const LogoutPage=async()=>{
+  try {
+    const {data}=await logoutUser()
+    navigate("/")
+  } catch (error) {
+    console.log(error)
+    console.log(error.reponse)
+  }
+ }
+  
+
+
+  console.log('is', isLoggedUser)
+  return (
+    <div className="min-h-screen flex flex-col">
+      <header className='flex justify-between items-center p-6 text-lg border-b mb-80'>
+        <Link to='/'>Homepage</Link>
+        <div className='flex gap-6'>
+          <Link to='/jobs'>Jobs</Link>
+          {isLoggedUser.login == true && isLoggedUser.verify == true ? <>
+            <Link to='user//dashboard'>Dashboard</Link> 
+          </>
+          : isLoggedUser.login == true ? <Link to='/verify-email'>Verify</Link> : <>
+            <Link to='/signup'>Signup</Link>
+            <Link to='/login'>Login</Link>
+          </>}
+          {isLoggedUser.login==true && <Link to='/home' onClick={LogoutPage}>Logout</Link> }
+        </div>
+      </header>
+      <div className="p-6">
+        <Outlet />
+      </div>
+    </div>
+  )
 }
 
 export default App
